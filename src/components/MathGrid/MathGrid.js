a7ede0fc9214444b345deec49abb00cbbd54da15
joel_yoffe <joel.y.mobile@gmail.com>
2023-01-27 01:19:45 +1100

    rudimentary grid working, many errors

--- src/components/MathGrid/MathGrid.js
+++ src/components/MathGrid/MathGrid.js
@@ -2,6 +2,10 @@ import React from 'react';
 import 'tachyons';
 import { Stage, Layer, Rect, Circle, Line, Text } from 'react-konva';
 import Konva from 'konva'
+import {clamp, Vector, getLinePoints, testFunctionEvaluator} from '../../algorithms/utilities'
+import 'big-js';
+
+//Big.DP = 30
 
 class MathGrid extends React.Component {
   constructor(props) {
@@ -14,7 +18,8 @@ class MathGrid extends React.Component {
         center: [0,0] //centre of screen
       }
     }
-    this.containerDiv = React.createRef()
+    this.containerDiv = React.createRef();
+    this.mousePos = [0,0];
   }
   
   componentDidMount = () => {
@@ -49,90 +54,45 @@ class MathGrid extends React.Component {
   }
 
   calculateGridInfo = (gridSettings, posInfo) => {
+    const minGridSpacing = 20; //px
+    const maxGridSpacing = 40; //px
+    const idealGridSpacing = 30; //px
+
     let {scale, center} = gridSettings;
 
-    let minGridSpacing = 10; //px
-    let maxGridSpacing = 20; //px
 
-    let gridSpacing = 15 * 1/scale;
+    let gridSpacing = idealGridSpacing * 1/scale;
 
     let gridJump = 1;
-
+    //let gridJumpSteps = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 2, 4, 10, 20, 40, 80]
+    //let gridJumpStepIndex = gridJumpSteps.indexOf(1);
+    
     // calculate variables relevant to scaling both x and y of grid. can optimise to no loop and just a math equation later
     while (gridSpacing > maxGridSpacing) {
       gridSpacing /= 2;
       gridJump /= 2
+      //gridJumpStepIndex--;
     }
     while (gridSpacing < minGridSpacing) {
-      gridSpacing *=2;
+      gridSpacing *= 2;
       gridJump *= 2;
+      //gridJumpStepIndex++;
     }
+    //let gridJump = gridJumpSteps[clamp(gridJumpStepIndex, 0, gridJumpSteps.length-1)]
 
-    // also implement big lines with text every 5 later
 
     // X LINES
-    // calculate the earliest number, unit value, and pixel position (offset) of the earliest x grid line that appears on the screen
-    let {line, offset} = this.calculateEarliestLineFromAxis(center[0], posInfo.hw, gridSpacing)
-    // let screenBound;
-    // let line;
-    // let offset;
-    // if (center[0] <= posInfo.hw) {
-    //   screenBound = center[0] - posInfo.hw; // will be < 0
-    //   // find last multiple of gridSpacing that is > than ans. This is the first grid line on the screen
-    //   line = -1 * Math.floor(Math.abs(screenBound / gridSpacing)) // can simplify
-    //   offset = line*gridSpacing - screenBound// number of pixels to the right of the leftmost bound of canvas that gridline should be drawn
-    // }
-    // else {
-    //   screenBound = center[0] - posInfo.hw; // will be > 0
-    //   // find first multiple of gridSpacing that is > than ans. This is the first grid line on the screen
-    //   line = Math.ceil(screenBound / gridSpacing);
-    //   offset = line*gridSpacing - screenBound; // number of pixels to the right of the leftmost bound of canvas that gridline should be drawn
-    // }
-
-    let xLines = this.getGridLinesFromInitial(offset, line, gridJump, gridSpacing, posInfo.width)
-    // from this populate an array of pixel positions and their values for x lines
-    // let xLines= [];
-    // xLines.push({value: line*gridJump, pos: offset, lineNum:line});
-
-    // while (xLines[xLines.length-1].pos < posInfo.width) {
-    //   let xPrev = xLines[xLines.length-1];
-    //   xLines.push({value: xPrev.value+gridJump, pos: xPrev.pos+gridSpacing, lineNum: xPrev.lineNum+1});
-    // }
-    // xLines.pop(); // last element is over boundary
-
+    let earliestXLine = this.calculateEarliestLineFromAxis(center[0], posInfo.hw, gridSpacing)
+    let xLines = this.getGridLinesFromInitial(earliestXLine.offset, earliestXLine.line, gridJump, gridSpacing, posInfo.width)
     
     // Y LINES
-    // calculate the earliest number, unit value, and pixel position (offset) of the earliest y grid line that appears on the screen
+    let earliestYLine = this.calculateEarliestLineFromAxis(center[1], posInfo.hh, gridSpacing)
+    let yLines = this.getGridLinesFromInitial(earliestYLine.offset, earliestYLine.line, gridJump, gridSpacing, posInfo.height)
     
-    // reuse these variables. merge everything into a function later
-    // screenBound = 0;
-    // line = 0;
-    // offset = 0;
-    // if (center[0] <= posInfo.hw) {
-    //   screenBound = center[0] - posInfo.hw; // will be < 0
-    //   // find last multiple of gridSpacing that is > than ans. This is the first grid line on the screen
-    //   line = -1 * Math.floor(Math.abs(screenBound / gridSpacing)) // can simplify
-    //   offset = line*gridSpacing - screenBound// number of pixels to the right of the leftmost bound of canvas that gridline should be drawn
-    // }
-    // else {
-    //   screenBound = center[0] - posInfo.hw; // will be > 0
-    //   // find first multiple of gridSpacing that is > than ans. This is the first grid line on the screen
-    //   line = Math.ceil(screenBound / gridSpacing);
-    //   offset = line*gridSpacing - screenBound; // number of pixels to the right of the leftmost bound of canvas that gridline should be drawn
-    // }
+    
+    // also implement big lines with text every 5 later
 
-
-    // // from this populate an array of pixel positions and their values for x lines
-    // let xLines= [];
-    // xLines.push({value: line*gridJump, pos: offset, lineNum:line});
-
-    // while (xLines[xLines.length-1].pos < posInfo.width) {
-    //   let xPrev = xLines[xLines.length-1];
-    //   xLines.push({value: xPrev.value+gridJump, pos: xPrev.pos+gridSpacing, lineNum: xPrev.lineNum+1});
-    // }
-    // xLines.pop(); // last element is over boundary
-
-    return {xLines: xLines};
+    return {xLines: xLines, yLines: yLines, unitScreenBounds: this.calculateUnitScreenBounds(center, gridSpacing, gridJump, posInfo.hw, posInfo.hh)};
   }
 
   getGridLinesFromInitial = (initialOffset, initialLineNum, gridJump, gridSpacing, maxLength) => {
@@ -147,7 +107,17 @@ class MathGrid extends React.Component {
     return lines
   }
 
+  calculateUnitScreenBounds = (center, gridSpacing, gridJump, xHalfAxis, yHalfAxis) => {
+    return {
+      left: (center[0] - xHalfAxis) / gridSpacing * gridJump,
+      right: (center[0] + xHalfAxis) / gridSpacing * gridJump,
+      bottom: (-center[1] - yHalfAxis) / gridSpacing * gridJump, //why - ?? i think i screwed up how vertical center works. !!!REDO LATER.
+      top: (-center[1] + yHalfAxis) / gridSpacing * gridJump,
+    }
+  }
+
   calculateEarliestLineFromAxis = (centerVal, halfAxis, gridSpacing) => {
+    // calculate the earliest number, unit value, and pixel position (offset) of the earliest x grid line that appears on the screen
     let screenBound, line, offset;
 
     if (centerVal <= halfAxis) {
@@ -166,18 +136,53 @@ class MathGrid extends React.Component {
   }
 
   handleWheel = (e) => {
-    const SPEED = 0.01; // add some sort of modifiier because nearer to 0.01 it gets crazy fast. you need to slow speed as num gets smaller, and increase it as num gets bigger.
+    const SPEED_MULTIPLIER = 0.01;
 
+    // as scale gets very small, speed gets smaller as well. as scale very large, speed very large.
+    // could set speed = 0
     // will also need to make it scale on mousewheel somehow.
 
+    // calculate how far centre will need to be moved to look like grid zooming in on mouse
+
+
     let nativeEvent = e.evt
-    this.setState((prevState) => ({
-      gridSettings: {
-        ...prevState.gridSettings,
-        scale: Math.max(0.01, prevState.gridSettings.scale + nativeEvent.deltaY * SPEED)
+    this.setState((prevState) => {
+      let speed = SPEED_MULTIPLIER * prevState.gridSettings.scale
+      let newScale = Math.max(0.001, prevState.gridSettings.scale + nativeEvent.deltaY * speed);
+
+      // get mouse pos from centre
+      let mouseX = prevState.gridSettings.center[0] - prevState.width/2 + this.mousePos[0] ;
+      let mouseY = prevState.gridSettings.center[1] - prevState.height/2 + this.mousePos[1];
+      let mouseToCentreVec = new Vector(mouseX, mouseY);
+      
+
+      // center[0] + (center[0] - prevState.width/2 + this.mousePos[0]) * nativeEvent.deltaY * speedMultiplier
+
+      console.log(prevState.gridSettings.center, mouseToCentreVec)
+      // scale to offset size
+      // (prevScale + nativeEvent.deltaY * SPEED_MULTIPLIER * prevScale) / prevScale - 1
+      // 
+      //let s = (new Big(newScale)).div(new Big(prevState.gridSettings.scale)).minus(new Big(1))
+      //mouseToCentreVec.scale(s)//newScale / prevState.gridSettings.scale - 1)
+      
+      if (newScale !== 0.001){
+        mouseToCentreVec.scale(nativeEvent.deltaY * SPEED_MULTIPLIER); // this is simplified version of newScale/oldscale -1
       }
-    }))
-    console.log("scale", (this.state.gridSettings.scale).toFixed(2), "deltaY", nativeEvent.deltaY.toFixed(2))
+      else {
+        mouseToCentreVec.scale(0);
+      }
+      mouseToCentreVec.flip()
+
+      //let screenCentreVec = new Vector(prevState.width/2, prevState.height/2);
+      let newCentre = [prevState.gridSettings.center[0] + mouseToCentreVec.x, prevState.gridSettings.center[1] + mouseToCentreVec.y]
+      // console.log(mouseToCentreVec)
+      return {gridSettings: {
+        ...prevState.gridSettings,
+        scale: newScale,
+        center: newCentre
+      }}
+    })
+   // console.log("scale", (this.state.gridSettings.scale).toFixed(2), "deltaY", nativeEvent.deltaY.toFixed(2))
   }
 
   handleMouseMove = (e) => {
@@ -196,7 +201,16 @@ class MathGrid extends React.Component {
       }))
     }
     
+    // allow other methods to access mouse position within canvas
+    
+    
+    let rect = nativeEvent.target.getBoundingClientRect()
+    this.mousePos = [nativeEvent.clientX - rect.left, nativeEvent.clientY - rect.top]
+
+    // console.log(nativeEvent.x - rect.left, nativeEvent.screenX)
+
     // console.log("scale", (this.state.gridSettings.scale).toFixed(2), "deltaY", nativeEvent.deltaY.toFixed(2))
+    
   }
 
   render () {
@@ -204,13 +218,41 @@ class MathGrid extends React.Component {
     let posInfo = this.calculatePosInfo(width, height)
     let gridInfo = this.calculateGridInfo(gridSettings, posInfo);
 
+    let yAxis = gridInfo.xLines.filter((line) => line.lineNum === 0)[0] // if main X line is not on the screen, this is undefined
+    let xAxis = gridInfo.yLines.filter((line) => line.lineNum === 0)[0] // as above
+    
+
+    // these numbers are overkill
+    let xNumbersYPos = xAxis !== undefined ? xAxis.pos : (gridSettings.center[1] > 0 ? -height - 2: height + 2); // the 2nd tertiary operator checks if x axis is below or above screen. If it is below, x nums should stick to bottom of screen. Otherwise, to top.
+    xNumbersYPos = clamp(xNumbersYPos, 10, height-20)
+
     // make a list of x gridlines
     //let xGridLines = [];
     let xGridLines = gridInfo.xLines.map((item) => {
-      return <Line points={[item.pos,0,item.pos,height]} stroke={"black"} strokeWidth={item.lineNum === 0 ? 2 : 0.3} key={item.pos}/>
+     // console.log(item.value)
+      // see react conditional rendering docs for explanation of the Text && bit.
+      let strokeWidth = 0.3;
+      if (item.lineNum % 5 == 0) {
+        strokeWidth = 0.6;
+      }
+      if (item.lineNum == 0) {
+        strokeWidth = 2;
+      }
+
+      return (
+        <div>
+          {(item.lineNum % 5 == 0 && item.lineNum !== 0) && <Text verticalAlign={"middle"} align={"center"} x={item.pos-30} y={xNumbersYPos-10} width={60} height={20} text={item.value.toString()} 
+          fontSize={10} fontFamily={'Calibri'} fill={'black'}/>} 
+          <Line points={[item.pos,0,item.pos,height]} stroke={"black"} strokeWidth={strokeWidth} key={item.pos}/>
+        </div>      
+      )
+    })
+    let yGridLines = gridInfo.yLines.map((item) => {
+      return <Line points={[0,item.pos,width,item.pos]} stroke={"black"} strokeWidth={item.lineNum === 0 ? 2 : 0.3} key={item.pos}/>
     })
 
-    //console.log(xGridLines)
+    
+
     return (
       <div ref={this.containerDiv} className='w-100 h-100' style={{position:'relative'}}>
         <Stage 
@@ -222,6 +264,13 @@ class MathGrid extends React.Component {
         >
           <Layer >
             {xGridLines}
+            {yGridLines}
+            <Line points={getLinePoints(width,height,
+              gridInfo.unitScreenBounds.left,
+              gridInfo.unitScreenBounds.right,
+              gridInfo.unitScreenBounds.bottom,
+              gridInfo.unitScreenBounds.top, 
+              testFunctionEvaluator)} strokeWidth={1} stroke={"red"}/>
             {/* <Line points={[0,height/2,width,height/2]} stroke={"black"} strokeWidth={2}/>
             <Line points={[width/2,0,width/2,height]} stroke={"black"} strokeWidth={2}/> */}
           </Layer>
--- src/components/grid.js
+++ src/components/grid.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Grid(props) {
     let { pixelArray } = props;
-
+    
     return (
         <div>
             <canvas />
